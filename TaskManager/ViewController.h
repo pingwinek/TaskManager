ba0d28a0a7b1f2dd64acf1b6bdf0cf9064739893
pingwinek <gornik.lukasz@gmail.com>
2016-10-08 13:01:21 +0200

    Initial Commit

---
+++ TaskManager/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TaskManager
+//
+//  Created by mac on 08.10.2016.
+//  Copyright © 2016 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TaskManager/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  TaskManager
+//
+//  Created by mac on 08.10.2016.
+//  Copyright © 2016 mac. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
